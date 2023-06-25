f7fe1c72e3babaa33e741ca198761384996f7ee9
jiyoon <vywns9978@gmail.com>
2023-06-25 15:13:16 +0900

    [🎨  style] 오늘의 일기 섹션 스타일 적용

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 import MyButton from "./MyButton";
 import MyHeader from "./MyHeader";
@@ -37,6 +37,8 @@ const getStringDate = (date) => {
 };
 
 const DiaryEditor = () => {
+  const contentRef = useRef();
+  const [content, setContent] = useState("");
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
@@ -76,6 +78,17 @@ const DiaryEditor = () => {
               isSelected={it.emotion_id === emotion}
             />
           ))}
+        </div>
+      </section>
+      <section>
+        <h4>오늘의 일기</h4>
+        <div className="input_box text_wrapper">
+          <textarea
+            placeholder="오늘은 어땠나요?"
+            ref={contentRef}
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
       </section>
     </div>
